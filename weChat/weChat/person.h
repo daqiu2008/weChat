881af2c38b0b1c8307c24efe7ec04c5b0da47735
Daqiu <zhangdaqiu@evbao.com.cn>
2018-12-20 01:39:11 +0800

    创建了person类

---
+++ weChat/weChat/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  weChat
+//
+//  Created by daqiu2016 on 2018/12/20.
+//  Copyright © 2018年 Grea Space. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ weChat/weChat/person.m
@@ -0,0 +1,13 @@
+ //
+//  person.m
+//  weChat
+//
+//  Created by daqiu2016 on 2018/12/20.
+//  Copyright © 2018年 Grea Space. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
